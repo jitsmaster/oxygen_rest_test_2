46ebca090c5981d27632d77f22c9096a2f72b100
Arnold Wang <Arnold.Wang@ingeniux.com>
2018-11-07 15:38:50 -0800

    Test save working

--- TestOxygen/Controllers/ValuesController.cs
+++ TestOxygen/Controllers/ValuesController.cs
@@ -6,42 +6,57 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
+using System.Xml.Linq;
 
 namespace TestOxygen.Controllers
 {
 	public class ValuesController : ApiController
 	{
+		private const string filePath = @"c:\dev\r_low_level_back_ends.xml";
+
 		protected override void Initialize(HttpControllerContext controllerContext)
 		{
 			base.Initialize(controllerContext);
 		}
 
 		[HttpGet]
+		[HttpPut]
 		public HttpResponseMessage Files(string url)
 		{
-			string fileContent = "";
-			using(var sr = new StreamReader(@"c:\dev\r_low_level_back_ends.xml"))
+			if (Request.Method.Method.ToLowerInvariant() != "put")
 			{
-				fileContent = sr.ReadToEnd();
-			}
-
-			// write the response body, set the file name and content-type
-			var result = new HttpResponseMessage(HttpStatusCode.OK)
-			{
-				Content = new ByteArrayContent(Encoding.UTF8.GetBytes(fileContent))
-			};
-			result.Content.Headers.ContentDisposition =
-				new ContentDispositionHeaderValue("attachment")
+				string fileContent = "";
+				using (var sr = new StreamReader(filePath))
 				{
-					FileName = url.Substring(7)
+					fileContent = sr.ReadToEnd();
+				}
+
+				// write the response body, set the file name and content-type
+				var result = new HttpResponseMessage(HttpStatusCode.OK)
+				{
+					Content = new ByteArrayContent(Encoding.UTF8.GetBytes(fileContent))
 				};
+				result.Content.Headers.ContentDisposition =
+					new ContentDispositionHeaderValue("attachment")
+					{
+						FileName = url.Substring(7)
+					};
 
-			result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+				result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
-			return result;
+				return result;
+			}
+			else
+			{
+				XDocument doc = XDocument.Load(HttpContext.Current.Request.InputStream);
+
+				doc.Save(filePath);
+
+				return new HttpResponseMessage(HttpStatusCode.OK);
+			}
 		}
-
 	}
 }
